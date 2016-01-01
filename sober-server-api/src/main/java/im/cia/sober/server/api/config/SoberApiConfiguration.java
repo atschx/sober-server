7a08f6f9030c2a7163077f27c713d8f39984e461
Albert <atschx@gmail.com>
2016-01-01 15:33:15 +0800

    configuration update

--- sober-server-api/src/main/java/im/cia/sober/server/api/config/SoberApiConfiguration.java
+++ sober-server-api/src/main/java/im/cia/sober/server/api/config/SoberApiConfiguration.java
@@ -1,4 +1,4 @@
-package im.cia.sober.server.api.configuration;
+package im.cia.sober.server.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,15 +6,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 /**
- * 跨域支持
+ * 此处统一配置SoberApi
  */
 @Configuration
-public class SoberApiCORSConfiguration {
+public class SoberApiConfiguration {
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurerAdapter() {
+			
+			/**
+			 * 跨域支持
+			 */
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**");
