8486bbe28e7b50aacc0df3f51292652762bd8404
Albert Sun <atschx@gmail.com>
2016-01-20 16:14:33 +0800

    users paged list

--- sober-server-adnetwork/src/main/java/com/atschx/adnetwork/web/UserController.java
+++ sober-server-adnetwork/src/main/java/com/atschx/adnetwork/web/UserController.java
@@ -1,5 +1,7 @@
 package com.atschx.adnetwork.web;
 
+import javax.servlet.ServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,37 +17,45 @@ import com.atschx.adnetwork.domain.repository.UserRepository;
 
 @RestController
 public class UserController extends AdNetworkController {
-	
+
+	private final UserRepository soberUserRepository;
+
 	@Autowired
-	UserRepository soberUserRepository;
-
-	@RequestMapping(value = "/user/reset")
-	String resetPassword(String email) {
-
-		// ret = 0,密码已发送至xx邮箱
-
-		return "reset success";
+	public UserController(UserRepository soberUserRepository) {
+		this.soberUserRepository = soberUserRepository;
 	}
 
-	// status=0&manager=-1&name=m&limit=20&page=1
+	/**
+	 * 查询广告主.
+	 * 
+	 * @param page
+	 * @param size
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping(value = "/advertisers", method = { RequestMethod.GET })
-	String advertisers(@RequestParam(name = "status") Byte status,
-			@RequestParam(name = "manager", required = false) Long manager,
-			@RequestParam(name = "name", required = false) String name,
-			@RequestParam(name = "page", defaultValue = "0") Integer page,
-			@RequestParam(name = "size", defaultValue = "20") Integer size) {
+	public String advertisers(
+			@PageableDefault(value = 15, sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable,
+			ServletRequest request) {
 		
+		//获取请求中的参数信息
 		
+
+		// 统一处理查询条件
+		//
+		// * @param status
+		// * @param manager
+		// * @param name
 
 		return "";
 	}
 
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public Page<User> getEntryByPageable(
+	public Page<User> users(
 			@PageableDefault(value = 15, sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable,
 			@RequestParam(value = "role", defaultValue = "") String role) {
-		
-		if("".equals(role)){ 
+
+		if ("".equals(role)) {
 			return soberUserRepository.findAll(pageable);
 		}
 
