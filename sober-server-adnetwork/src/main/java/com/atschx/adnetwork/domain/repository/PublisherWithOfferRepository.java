3b4cfe8e841e18fdf6342fa6591cf202efaff991
Albert Sun <atschx@gmail.com>
2016-01-26 18:51:02 +0800

    add offer publish apply list

---
+++ sober-server-adnetwork/src/main/java/com/atschx/adnetwork/domain/repository/PublisherWithOfferRepository.java
@@ -0,0 +1,11 @@
+package com.atschx.adnetwork.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.atschx.adnetwork.domain.model.PublisherWithOffer;
+
+public interface PublisherWithOfferRepository extends  JpaRepository<PublisherWithOffer, Long> {
+
+	
+	
+}
---
+++ sober-server-adnetwork/src/main/java/com/atschx/adnetwork/protocol/response/ApplyResult.java
@@ -0,0 +1,17 @@
+package com.atschx.adnetwork.protocol.response;
+
+import com.atschx.adnetwork.protocol.Result;
+
+public class ApplyResult extends Result {
+
+	private Long aid;
+
+	public Long getAid() {
+		return aid;
+	}
+
+	public void setAid(Long aid) {
+		this.aid = aid;
+	}
+
+}
